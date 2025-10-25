f6dfe832cf4f93f8a000d4f2ffd3ebc1914a902c
Tushar Punia <tusharpunia1144@gmail.com>
2025-10-25 13:58:57 +0530

    add comment

--- src/main/java/QuizApp/project/ProjectApplication.java
+++ src/main/java/QuizApp/project/ProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
+//This is a main class
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
